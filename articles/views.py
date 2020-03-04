a466405b6e196d60fc1e26b412dda1bc884f94bd
Prasanth Karri <prasanth.25k@gmail.com>
2020-03-04 17:37:53 +0530

    auto add author field while creating

--- articles/views.py
+++ articles/views.py
@@ -28,5 +28,9 @@ class ArticleDeleteView(DeleteView):
 
 class ArticleCreateView(CreateView):
     model = Article
-    fields = ('title', 'body', 'author')
+    fields = ('title', 'body')
     template_name = 'article_new.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
